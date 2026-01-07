55207054d9263f381d7ecc12f3ce8bab2a30aa3a
Alex Diaz <j.a.diaz@utexas.edu>
2026-01-07 17:05:48 -0600

    [fix] Add preprocessing to predictor

--- tools/predictor.py
+++ tools/predictor.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from datasets.data_transforms import Compose
 
 class AdaPoinTrPredictor:
     def __init__(self, model: torch.nn.Module,
@@ -10,6 +11,17 @@ class AdaPoinTrPredictor:
         self.device = next(model.parameters()).device
         self.n_points = n_points
         self.normalize = normalize
+
+        self.transform = Compose([{
+            'callback': 'UpSamplePoints',
+            'parameters': {
+                'n_points': self.n_points
+            },
+            'objects': ['input']
+        }, {
+            'callback': 'ToTensor',
+            'objects': ['input']
+        }])
 
     def predict(self,
                 point_cloud: np.ndarray # Input partial point cloud
@@ -23,12 +35,23 @@ class AdaPoinTrPredictor:
         Returns:
             complete_pc: (M, 3) predicted complete point cloud
         """
+        point_cloud = np.asarray(point_cloud, dtype=np.float32)
         assert point_cloud.ndim == 2 and point_cloud.shape[1] == 3, "Input point cloud must be of shape (N, 3)"
 
-        with torch.no_grad():
-            xyz = torch.from_numpy(point_cloud).float().unsqueeze(0).to(self.device) # (1, N, 3)
+        if self.normalize:
+            centroid = np.mean(point_cloud, axis=0)
+            point_cloud = point_cloud - centroid
+            m = np.max(np.sqrt(np.sum(point_cloud**2, axis=1)))
+            point_cloud = point_cloud / (m + 1e-8)
 
-            coarse, fine = self.model(xyz) # fine: (1, M, 3)
+        pc_ndarray_normalized = self.transform({'input': point_cloud})
+
+        with torch.no_grad():
+            coarse, fine = self.model(pc_ndarray_normalized['input'].unsqueeze(0).to(self.device)) # fine: (1, M, 3)
             complete_pc = fine.squeeze(0).cpu().numpy() # (M, 3)
+
+        if self.normalize:
+            complete_pc = complete_pc * (m + 1e-8)
+            complete_pc = complete_pc + centroid
 
         return complete_pc
