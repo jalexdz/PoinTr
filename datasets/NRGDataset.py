[fix] Correct normalization

datasets/NRGDataset.py
@@ -87,12 +87,19 @@ class NRG(data.Dataset):
         return pc[idx].astype(np.float32)
 
     def _norm_like_shapenet_using_gt(self, gt: np.ndarray, partial: np.ndarray):
-        centroid = np.mean(gt, axis=0)
-        gt0 = gt - centroid
-        m = np.max(np.sqrt(np.sum(gt0**2, axis=1)))
-        scale = (m + 1e-12)
-        gt0 = gt0 / scale
-        partial0 = (partial - centroid) / scale
+        # Normalize from PARTIAL to GT
+        centroid = np.mean(partial, axis=0)
+        p0 = partial - centroid
+        scale = np.max(np.linalg.norm(p0, axis=1)) + 1e-12
+
+        partial0 = p0 / scale
+        gt0 = (gt - centroid) / scale
+
+        # gt0 = gt - centroid
+        # m = np.max(np.sqrt(np.sum(gt0**2, axis=1)))
+        # scale = (m + 1e-12)
+        # gt0 = gt0 / scale
+        # partial0 = (partial - centroid) / scale
         return gt0.astype(np.float32), partial0.astype(np.float32)
 
     def __getitem__(self, idx):

tools/predictor.py
@@ -43,7 +43,7 @@ class AdaPoinTrPredictor:
             centroid = np.mean(point_cloud, axis=0)
             point_cloud = point_cloud - centroid
             m = np.max(np.sqrt(np.sum(point_cloud**2, axis=1)))
-            point_cloud = point_cloud / (m + 1e-8)
+            point_cloud = point_cloud / (m + 1e-12)
 
         pc_ndarray_normalized = self.transform({'input': point_cloud})
 
@@ -52,7 +52,7 @@ class AdaPoinTrPredictor:
             complete_pc = fine.squeeze(0).cpu().numpy() # (M, 3)
 
         if self.normalize:
-            complete_pc = complete_pc * (m + 1e-8)
+            complete_pc = complete_pc * (m + 1e-12)
             complete_pc = complete_pc + centroid
 
         return complete_pc